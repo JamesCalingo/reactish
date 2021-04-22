6b80b29df3fbc485d77931be5d88755021841d59
JC <james.calingo@gmail.com>
2021-04-22 13:31:50 -0400

    working on tests

--- src/js/Reactish.test.js
+++ src/js/Reactish.test.js
@@ -1,5 +1,9 @@
 const Reactish = require("./Reactish")
 
+it("Creates HTML elements and adds them to the DOM", () => {
+
+})
+
 it("returns object", () => {
   // const element = createElement("div", null, "CHILDREN")
   // expect(typeof element).toBe("object")
@@ -14,5 +18,5 @@ it("creates a DOM from the fibers", () => {
 })
 
 it("removes old DOM properties and events and inserts new DOM properties and events", () => {
-  
+
 })

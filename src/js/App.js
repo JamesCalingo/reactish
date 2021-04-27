a4f2dd5a9313456cd4ec9e44f0c6c06c5deac4b2
JC <james.calingo@gmail.com>
2021-04-27 17:02:10 -0400

    working on blog components

---
+++ src/js/App.js
@@ -0,0 +1,9 @@
+function App(props) {
+  return(
+    <div>
+      {props.forEach(post => {
+        <h1><a href={props.link}>{post.title}</a></h1>
+      })}
+    </div>
+  )
+}
--- src/js/Post.js
+++ src/js/Post.js
@@ -1,9 +1,9 @@
 function Post(props) {
   return (
     <div className="post">
-      <h1 className="post-title"></h1>
-      <h2 className="post-date"></h2>
-      <div className="post-body"></div>
+      <h1 className="post-title">{props.title}</h1>
+      <h2 className="post-date">{props.date}</h2>
+      <div className="post-body">{props.body}</div>
     </div>
   );
 }
